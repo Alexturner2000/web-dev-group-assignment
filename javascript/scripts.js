bef0ece6959841833f9274d0a5e9ca05ea6d6903
Alex Turner <67073439+Alexturner2000@users.noreply.github.com>
2021-03-22 00:02:17 -0400

    Add files via upload

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -13,3 +13,22 @@ function stickyHeader() {
     header.classList.remove("sticky");
   }
 }
+
+function initMap() {
+  // The location of Uluru
+  const uluru = {
+    lat: -25.344,
+    lng: 131.036
+    //50.06013859607651, -122.95661669464884
+  };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
